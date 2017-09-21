b6499f7c8a6b4acf370db52c15f841e4173b941b
Vivek Hegade M S <vivekhegade0407@gmail.com>
2017-09-21 20:05:55 +0530

    my code for tabs
    Not navigating may be i have missed to install some packages  i have pushed even the required files where i need to change

--- App.jsx
+++ App.jsx
@@ -2,44 +2,55 @@ import React from 'react';
 import TextField from 'material-ui/TextField';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
+import PropTypes from 'prop-types';
+
+
+
+import {List, ListItem} from 'material-ui/List';
+import ContentInbox from 'material-ui/svg-icons/content/inbox';
+import ActionGrade from 'material-ui/svg-icons/action/grade';
+import ContentSend from 'material-ui/svg-icons/content/send';
+import ContentDrafts from 'material-ui/svg-icons/content/drafts';
+import Divider from 'material-ui/Divider';
+import ActionInfo from 'material-ui/svg-icons/action/info';
+
+import {Tabs, Tab} from 'material-ui/Tabs';
+
 class App extends React.Component {
-    constructor() {
-        super();
-        this.state = {items: []}
-    }
-
-    componentWillMount() {
-        fetch('http://swapi.co/api/people/?format=json')
-            .then(response => response.json())
-            .then(({results: items}) => this.setState({items}))
-    }
-
-    filter(e) {
-        this.setState({filter: e.target.value})
-    }
-
-    render() {
-        let items = this.state.items;
-        if (this.state.filter) {
-            items = items.filter(item =>
-                item.name.toLowerCase()
-                    .includes(this.state.filter.toLowerCase()))
-        }
-        return (
-            <MuiThemeProvider>
-                <div>
-                    <h1>List Of the Names of the People</h1>
-                    <TextField hintText="Hint Text"/><br/>
-                    <input type="text"
-                           onChange={this.filter.bind(this)}/>
-                    {items.map(item =>
-                        <Person key={item.name} person={item}/>)}
-                </div>
-            </MuiThemeProvider>
-        );
+   render() {
+		
+		return (
+		 <MuiThemeProvider>
+			<div>
+				<h1>List Of the Names of the People</h1>
+				
+				<Tabs>
+				<Tab label="First Task" >
+				<div>
+				<List>
+					<ListItem primaryText="Luke Skywalker" rightIcon={<ActionInfo />} />
+					<ListItem primaryText="C-3PO" rightIcon={<ActionInfo />} />
+					<ListItem primaryText="R2-D2" rightIcon={<ActionInfo />} />
+					<ListItem primaryText="Darth Vader" rightIcon={<ActionInfo />} />
+				
+					<ListItem primaryText="Leia Organa" rightIcon={<ActionInfo />} />
+					<ListItem primaryText="Owen Lars" rightIcon={<ActionInfo />} />
+					<ListItem primaryText="Beru Whitesun lars" rightIcon={<ActionInfo />} />
+					<ListItem primaryText="R5-D4" rightIcon={<ActionInfo />} />
+				
+					<ListItem primaryText="Biggs Darklighter" rightIcon={<ActionInfo />} />
+					<ListItem primaryText="Obi-Wan Kenobi" rightIcon={<ActionInfo />} />
+				</List>
+				</div>
+				</Tab>
+				<Tab label="Second Tab" >
+				</Tab>
+			</Tabs>
+			</div>	
+		 </MuiThemeProvider> 
+		
+		);
     }
 }
-
-const Person = (props) => <h4>{props.person.name}</h4>
 
 export default App;
